d884011d93bb1a83dd711c8cbb39cbe46cfde45d
HappySplunker <roman.samarskyi@accenture.com>
2018-09-26 00:51:13 +0300

    First commit

---
+++ 1/WebContent/WEB-INF/view/list-products.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>List Products</title>
+</head>
+
+<body>
+
+List Products - coming soon ...
+
+</body>
+
+</html>
---
+++ 1/src/com/roman/spring/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.roman.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+	@RequestMapping("/list")
+	public String listProducts(Model theModel) {
+		
+		return "list-products";
+	}
+}
